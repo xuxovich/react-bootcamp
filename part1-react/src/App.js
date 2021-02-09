dd231e1982bbf3a2c55bbbdd716d17b2b63d7724
Andrew <andrescantosbonete@gmail.com>
2021-02-09 21:09:02 +0100

    part1

---
+++ part1-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Mensaje msg='Estamos trabajando con props'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ part1-react/src/Mensaje.js
@@ -0,0 +1,7 @@
+const Mensaje = (props) => {
+    return (
+        <h1>{props.msg}</h1>
+    );
+}
+
+export default Mensaje
